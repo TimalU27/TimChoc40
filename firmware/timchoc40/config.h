305d3457fc0d61cf396bf0967bba37e15c05afaa
Andrew Lin <landrew051127@gmail.com>
2024-01-01 06:15:16 +0800

    added wpm display and .hex file

--- firmware/timchoc40/config.h
+++ firmware/timchoc40/config.h
@@ -18,3 +18,5 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
+#define OLED_FADE_OUT true
--- firmware/timchoc40/rules.mk
+++ firmware/timchoc40/rules.mk
@@ -1,3 +1,12 @@
 # This file intentionally left blank
+
+#Oled setting
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
+
+
+#VIA setting
+VIA_ENABLE = yes
+
+#wpm setting 
+WPM_ENABLE = yes
